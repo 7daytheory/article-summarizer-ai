f9806651b0439ce6bd04a57615c19370301cfb28
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-05-19 14:47:45 -0400

    Updated styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { logo } from '../assets';
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-        <nav className="flex justify-between items-center flex-col">
+        <nav className="flex justify-between items-center  w-full mb-10 pt-3">
             <img src={logo} alt="SumZ Logo" className="w-28 object-contain" />
 
             <button
