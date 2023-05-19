522cff9089dd0114c181f5a55ef87f4853582b18
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-05-19 15:45:14 -0400

    Created form, added imported linkIcon inside input

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -6,7 +6,28 @@ import {copy, linkIcon, loader, tick} from '../assets'
 
 const Demo = () => {
   return (
-    <div>Demo</div>
+    <section className="mt-16 w-full max-w-xl">
+        {/* Search */}
+        <div className="flex flex-col w-full gap-2">
+            <form
+                className="relative flex justify-center items-center"
+                onSubmit={() => {}}
+            >
+                <img 
+                    src={linkIcon}
+                    className="absolute left-0 ml-3 w-5 my-2"
+                />
+                <input
+                    type="url"
+                    placeholder="Enter a URL"
+                    value=""
+                    onChange={() => {}}
+                    required
+                    className="url_input peer"
+                />
+            </form>
+        </div>
+    </section>
   )
 }
 
