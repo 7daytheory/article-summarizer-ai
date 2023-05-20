8e84297bb47b1a8167ba082658d742b2d7ca994a
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-05-20 18:30:17 -0400

    Setup rapidAPI Get call, documentation in README

--- src/services/article.js
+++ src/services/article.js
@@ -1,8 +1,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+
 export const articleApi = createApi({
     reducerPath: 'articleApi',
-    endpoints: (builder) => ({
+    baseQuery: fetchBaseQuery({
+       baseurl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+       prepareHeaders: (header) => {
+        headers.set('X-RapidAPI-Key', rapidApiKey);
+        headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+
+        return headers;
+       }
+    }),
+   endpoints: (builder) => ({
         getSummary: builder.query({
             query: (params) => 'test'
         })
